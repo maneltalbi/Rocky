51995424be88fcb6291e0cf8ded9681a564850bf
manel talbi <talbimanel20@gmail.com>
2022-01-17 13:23:44 +0100

    create database Rocky and table category

---
+++ rocky1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+using rocky1.Models;
+
+namespace rocky1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
